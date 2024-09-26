609feb89c3fb6018d2f7df9bd82a1e0cdc8ff738
Тарас Иванов <taras281@yandex.ru>
2024-09-26 12:45:49 +0200

    created

---
+++ src/test/java/com/github/javarushcommunity/springbootflywaydemo/SpringbootFlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javarushcommunity.springbootflywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
